4277fe2d76e06aedeae1ae00aea1d31ea2ca8710
Anca Dobrin <adobrin208@yahoo.com>
2024-03-12 15:40:19 +0200

    importarea moddulelor/docstring

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def get_todos(filepath):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(filepath, todos_arg):
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     #obtinem date de la utilizator si eliminam spatiile
@@ -16,20 +9,20 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:]
 
-        todos = get_todos("todo.txt")
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos("todo.txt", todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith('show'):
-         todos = get_todos("todo.txt")
+         todos = functions.get_todos()
 
          for index, item in enumerate(todos):
              item=item.strip('\n')
              row = f"{index + 1}-{item}"
              print(row)
-    elif  user_action.startswith('edit'):
+    elif user_action.startswith('edit'):
       try:
 
          number = int(user_action[5:])
@@ -37,12 +30,12 @@ while True:
 
          number = number-1
 
-         todos = get_todos("todo.txt")
+         todos = functions.get_todos()
 
          new_todo = input("Enter new todo ")
          todos[number] = new_todo + '\n'
 
-         write_todos("todo.txt", todos)
+         functions.write_todos(todos)
       except ValueError:
           print("Your command is not valid:")
           continue
@@ -51,12 +44,12 @@ while True:
       try:
           number = int(user_action[9:])
 
-          todos = get_todos("todo.txt")
+          todos = functions.get_todos()
           index= number -1
           todo_to_remove = todos[index].strip('\n')
           todos.pop(index)
 
-          write_todos("todo.txt", todos)
+          functions.write_todos(todos)
 
           message = f"Todo {todo_to_remove} was removed from the list"
           print(message)
--- test.py
+++ test.py
@@ -1,8 +1,4 @@
-waiting_list = ["john", "marry"]
-name = input("Enter name: ")
+def get_nr_items(user):
+    items= user.split(',')
+    return len(items)
 
-try:
-    number = waiting_list.index(name)
-    print(f"{name}'s turn is {number}")
-except ValueError:
-    print(f"{name} is not in the list.")
